13ac049aa2c9f7c1c2517ddcb3379afffe48281a
Surendar K <surendarkumar2k@gmail.com>
2026-03-04 17:20:40 +0530

    fix: undo last commit

--- backend/services/whatsapp/whatsappClient.js
+++ backend/services/whatsapp/whatsappClient.js
@@ -1,23 +1,21 @@
 import pkg from 'whatsapp-web.js';
 import qrcode from 'qrcode-terminal';
-import puppeteer from "puppeteer-core";
+
 const { Client, LocalAuth } = pkg;
-const browserFetcher = puppeteer.createBrowserFetcher({
-  path: "/opt/render/.cache/puppeteer",
-});
+
 const client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: {
-    executablePath: revisionInfo.executablePath,  // 👈 IMPORTANT
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-gpu"
-    ],
-    timeout: 60000
-  }
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: "new",   // important for newer Node versions
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu",
+            "--disable-features=IsolateOrigins,site-per-process"
+        ],
+        timeout: 60000
+    }
 });
 
 client.on('qr', (qr) => {
